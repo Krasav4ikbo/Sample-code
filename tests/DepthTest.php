d30cf12a78a9c946f3e055dbf1627670244dcb46
Bosyy <bosyypavel@gmail.com>
2018-06-19 12:26:22 +0300

    Added more tests

--- tests/DepthTest.php
+++ tests/DepthTest.php
@@ -22,7 +22,8 @@ class DepthTest extends TestCase
             ],
             [
                 'array' => [1,2,4,3,4],
-                'expected' => 1],
+                'expected' => 1
+            ],
             [
                 'array' => [5,2,4,3,4],
                 'expected' => 2
@@ -30,6 +31,34 @@ class DepthTest extends TestCase
             [
                 'array' => [1,2,3,8,2,35,2,4,3,4],
                 'expected' => 6
+            ],
+            [
+                'array' => [1,3,2,2,2,2,5,1,3,4,2],
+                'expected' => 3
+            ],
+            [
+                'array' => [3,2,1,2,3],
+                'expected' => 2
+            ],
+            [
+                'array' => [1,2,3,5,3,2,1],
+                'expected' => 0
+            ],
+            [
+                'array' => [1,2,3,5,3,4,1],
+                'expected' => 1
+            ],
+            [
+                'array' => [1,2,3,5,3,4,6],
+                'expected' => 2
+            ],
+            [
+                'array' => [10,9,8,7,6,5,4,4,5,3,2,1],
+                'expected' => 1
+            ],
+            [
+                'array' => [10,9,8,3,7,6,2,5,4,1,4,5,3,2,1],
+                'expected' => 4
             ],
         ];
     }
